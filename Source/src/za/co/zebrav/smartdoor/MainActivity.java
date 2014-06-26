bd3ee5b8a22bc1d0bdfe11688041f78bd32aebdb
Zuhnja <u12040593@tuks.co.za>
2014-06-26 16:39:00 +0200

    Updated comments

--- Source/src/za/co/zebrav/smartdoor/MainActivity.java
+++ Source/src/za/co/zebrav/smartdoor/MainActivity.java
@@ -1,11 +1,9 @@
 //Authors:
-//ZEBRA-V
 //Eduan Bekker - 12214834
 //Zuhnja Riekert - 12040593
 //Albert Volschenk - 12054519
 
 //This is the main activity for the Smart Door Application
-
 package za.co.zebrav.smartdoor;
 
 import android.app.Activity;
