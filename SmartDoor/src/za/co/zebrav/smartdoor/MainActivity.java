c5d16099aef6244b80cee631091275498e1249c1
eduanb <bekker.eduan@gmail.com>
2014-06-26 12:26:43 +0200

    First Commit

---
+++ SmartDoor/src/za/co/zebrav/smartdoor/MainActivity.java
@@ -0,0 +1,22 @@
+package za.co.zebrav.smartdoor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity
+{
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+}
