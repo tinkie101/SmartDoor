c11208078c810517212df8f8340fc42f7a5f235d
eduanb <bekker.eduan@gmail.com>
2014-06-26 23:37:53 +0200

    Started to add camera functions

--- Source/src/za/co/zebrav/smartdoor/MainActivity.java
+++ Source/src/za/co/zebrav/smartdoor/MainActivity.java
@@ -8,9 +8,14 @@
 package za.co.zebrav.smartdoor;
 
 import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity
 {
@@ -24,6 +29,46 @@ public class MainActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		if(!checkCameraHardware(this))
+		{
+			Toast msg = Toast.makeText(this, "Camera required", Toast.LENGTH_LONG);
+			msg.show();
+			finish();
+		}
+		else
+		{
+			Toast msg = Toast.makeText(this, "Camera found", Toast.LENGTH_LONG);
+			msg.show();
+			Camera camera = getCameraInstance();
+		}
 	}
-	
+
+	/** Check if this device has a camera */
+	private boolean checkCameraHardware(Context context)
+	{
+		if (context.getPackageManager().hasSystemFeature(
+				PackageManager.FEATURE_CAMERA))
+		{
+			// this device has a camera
+			return true;
+		} else
+		{
+			// no camera on this device
+			return false;
+		}
+	}
+
+	/** A safe way to get an instance of the Camera object. */
+	public static Camera getCameraInstance()
+	{
+		Camera c = null;
+		try
+		{
+			c = Camera.open(); // attempt to get a Camera instance
+		} catch (Exception e)
+		{
+			// Camera is not available (in use or does not exist)
+		}
+		return c; // returns null if camera is unavailable
+	}
 }
