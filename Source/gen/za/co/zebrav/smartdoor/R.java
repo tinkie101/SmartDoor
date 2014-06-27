2937c85edeb0f840fc79482c23caff7ec1f731d8
tinkie101 <tinkie101@live.co.za>
2014-06-27 20:59:15 +0200

    Fixed some warnings in the custom layout for the array adapter

--- Source/gen/za/co/zebrav/smartdoor/R.java
+++ Source/gen/za/co/zebrav/smartdoor/R.java
@@ -25,6 +25,7 @@ public final class R {
         public static final int list_twitter=0x7f030002;
     }
     public static final class string {
+        public static final int ProfileImage=0x7f040005;
         public static final int app_name=0x7f040000;
         public static final int goto_twitter=0x7f040002;
         public static final int hello_world=0x7f040001;
