8ff25d98a7057d95991f20260c64f73b7a9a9545
tinkie101 <tinkie101@live.co.za>
2014-06-27 12:10:54 +0200

    Added the first try at Twitter Integration.

--- Source/src/za/co/zebrav/smartdoor/MainActivity.java
+++ Source/src/za/co/zebrav/smartdoor/MainActivity.java
@@ -1,17 +1,15 @@
 //Authors:
-//ZEBRA-V
 //Eduan Bekker - 12214834
 //Zuhnja Riekert - 12040593
 //Albert Volschenk - 12054519
 
 //This is the main activity for the Smart Door Application
-
 package za.co.zebrav.smartdoor;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity
 {
@@ -25,6 +23,12 @@ public class MainActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+	
+	public void gotoTwitter(View v)
+	{
+		Intent intent = new Intent(this, TwitterActivity.class);
+		startActivity(intent);
 	}
 	
 }
