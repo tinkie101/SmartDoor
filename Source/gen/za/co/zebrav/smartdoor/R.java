0f2cdc08d272283d819947d800dddfa5a3efb00a
Zuhnja <u12040593@tuks.co.za>
2014-07-01 19:53:12 +0200

    Portait layout changed

--- Source/gen/za/co/zebrav/smartdoor/R.java
+++ Source/gen/za/co/zebrav/smartdoor/R.java
@@ -16,11 +16,11 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
+        public static final int LinearLayout1=0x7f070000;
         public static final int action_refresh_tweets=0x7f070008;
-        public static final int button1=0x7f070002;
-        public static final int button_goto_twitter=0x7f070001;
+        public static final int button1=0x7f070001;
+        public static final int button_goto_twitter=0x7f070002;
         public static final int camera_preview=0x7f070003;
-        public static final int textView1=0x7f070000;
         public static final int twitterPostDateTime=0x7f070007;
         public static final int twitterUserHandle=0x7f070005;
         public static final int twitterUserImage=0x7f070004;
