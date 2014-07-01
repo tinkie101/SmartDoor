5be430328a4b7f75c31b51299dc641b868ddce9b
Zuhnja <u12040593@tuks.co.za>
2014-07-01 19:19:11 +0200

    Landscape layout

--- Source/gen/za/co/zebrav/smartdoor/R.java
+++ Source/gen/za/co/zebrav/smartdoor/R.java
@@ -11,8 +11,9 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_refresh=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int background=0x7f020000;
+        public static final int ic_action_refresh=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
         public static final int action_refresh_tweets=0x7f070008;
