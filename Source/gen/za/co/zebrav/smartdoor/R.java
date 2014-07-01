dbcf3ae1bf321927ac5536e8942d195a5c1f7658
Zuhnja <u12040593@tuks.co.za>
2014-07-01 13:14:35 +0200

    Updated R

--- Source/gen/za/co/zebrav/smartdoor/R.java
+++ Source/gen/za/co/zebrav/smartdoor/R.java
@@ -13,17 +13,8 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
-    public static final class id {
-        public static final int manualLogin=0x7f070001;
-        public static final int option2=0x7f070002;
-        public static final int textView1=0x7f070000;
-    }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int manual_login=0x7f030001;
-    }
-    public static final class menu {
-        public static final int mymenu=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
