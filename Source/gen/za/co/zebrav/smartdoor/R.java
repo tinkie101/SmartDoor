aa0426a0ece82264397cabc895efb11f03114183
Zuhnja <u12040593@tuks.co.za>
2014-07-22 11:45:36 +0200

    Fixed volume error

--- Source/gen/za/co/zebrav/smartdoor/R.java
+++ Source/gen/za/co/zebrav/smartdoor/R.java
@@ -21,8 +21,8 @@ public final class R {
         public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int SpeakButton=0x7f080012;
         public static final int action_refresh_tweets=0x7f080013;
+        public static final int btnSpeak=0x7f080012;
         public static final int buttonLinear=0x7f080002;
         public static final int cameraFragment=0x7f080005;
         public static final int camera_preview=0x7f08000b;
@@ -35,12 +35,12 @@ public final class R {
         public static final int stt=0x7f080004;
         public static final int textView1=0x7f080010;
         public static final int tts=0x7f080003;
-        public static final int ttsEditText=0x7f080011;
         public static final int twitterFragment=0x7f080006;
         public static final int twitterPostDateTime=0x7f08000f;
         public static final int twitterUserHandle=0x7f08000d;
         public static final int twitterUserImage=0x7f08000c;
         public static final int twitterUserText=0x7f08000e;
+        public static final int txtText=0x7f080011;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
