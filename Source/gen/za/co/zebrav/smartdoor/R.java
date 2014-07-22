a32e547a45a1a57477868ac68ebba9ad230997b1
tinkie101 <tinkie101@live.co.za>
2014-07-22 11:27:16 +0200

    Started with recognito integration

--- Source/gen/za/co/zebrav/smartdoor/R.java
+++ Source/gen/za/co/zebrav/smartdoor/R.java
@@ -21,36 +21,38 @@ public final class R {
         public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int SpeakButton=0x7f080012;
-        public static final int action_refresh_tweets=0x7f080013;
+        public static final int SpeakButton=0x7f080013;
+        public static final int action_refresh_tweets=0x7f080014;
         public static final int buttonLinear=0x7f080002;
-        public static final int cameraFragment=0x7f080005;
-        public static final int camera_preview=0x7f08000b;
+        public static final int cameraFragment=0x7f080006;
+        public static final int camera_preview=0x7f08000c;
         public static final int content_frame=0x7f080001;
         public static final int drawer_layout=0x7f080000;
-        public static final int drawer_list=0x7f080007;
-        public static final int listenSpeech=0x7f080008;
-        public static final int speechToTextList=0x7f08000a;
-        public static final int speech_loadingBar=0x7f080009;
+        public static final int drawer_list=0x7f080008;
+        public static final int listenSpeech=0x7f080009;
+        public static final int speechToTextList=0x7f08000b;
+        public static final int speech_loadingBar=0x7f08000a;
         public static final int stt=0x7f080004;
-        public static final int textView1=0x7f080010;
+        public static final int textView1=0x7f080011;
         public static final int tts=0x7f080003;
-        public static final int ttsEditText=0x7f080011;
-        public static final int twitterFragment=0x7f080006;
-        public static final int twitterPostDateTime=0x7f08000f;
-        public static final int twitterUserHandle=0x7f08000d;
-        public static final int twitterUserImage=0x7f08000c;
-        public static final int twitterUserText=0x7f08000e;
+        public static final int ttsEditText=0x7f080012;
+        public static final int twitterFragment=0x7f080007;
+        public static final int twitterPostDateTime=0x7f080010;
+        public static final int twitterUserHandle=0x7f08000e;
+        public static final int twitterUserImage=0x7f08000d;
+        public static final int twitterUserText=0x7f08000f;
+        public static final int voiceIdentification=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_speech_to_text=0x7f030001;
-        public static final int camera_layout=0x7f030002;
-        public static final int drawer_list_item=0x7f030003;
-        public static final int fragment_twitter=0x7f030004;
-        public static final int list_twitter=0x7f030005;
-        public static final int manual_login=0x7f030006;
-        public static final int texttospeech=0x7f030007;
+        public static final int activity_voice_identification=0x7f030002;
+        public static final int camera_layout=0x7f030003;
+        public static final int drawer_list_item=0x7f030004;
+        public static final int fragment_twitter=0x7f030005;
+        public static final int list_twitter=0x7f030006;
+        public static final int manual_login=0x7f030007;
+        public static final int texttospeech=0x7f030008;
     }
     public static final class menu {
         public static final int twitter=0x7f070000;
@@ -67,6 +69,7 @@ public final class R {
         public static final int placeholder=0x7f040004;
         public static final int title_activity_speech_to_text=0x7f04000a;
         public static final int title_activity_twitter=0x7f040003;
+        public static final int title_activity_voice_identification=0x7f04000b;
     }
     public static final class style {
         /** 
