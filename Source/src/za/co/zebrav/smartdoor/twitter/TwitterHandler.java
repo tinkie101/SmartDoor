49ae64c529f4049fd77ded8f4b40847cc8f755de
tinkie101 <tinkie101@live.co.za>
2014-08-19 11:43:52 +0200

    Moved the twitter classes around.

--- Source/src/za/co/zebrav/smartdoor/twitter/TwitterHandler.java
+++ Source/src/za/co/zebrav/smartdoor/twitter/TwitterHandler.java
@@ -1,17 +1,23 @@
 package za.co.zebrav.smartdoor.twitter;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import twitter4j.Status;
+import twitter4j.Paging;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 import za.co.zebrav.smartdoor.R;
+import android.R.anim;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.widget.ListView;
@@ -108,7 +114,7 @@ public class TwitterHandler
 		{
 			Toast.makeText(fragmentContext, "Updating Tweets", Toast.LENGTH_LONG).show();
 			fragmentContext.findViewById(R.id.twitter_refreshBar).setVisibility(ProgressBar.VISIBLE);
-			asynchTwitter = new AsynchTwitter(this);
+			asynchTwitter = new AsynchTwitter();
 			asynchTwitter.execute();
 		}
 		else
@@ -126,26 +132,329 @@ public class TwitterHandler
 		if (asynchTwitter != null)
 			asynchTwitter.cancel(true);
 	}
-
-	public void processFinish(List<Status> result)
+	
+	/**
+	 * @author tinkie101
+	 * 
+	 */
+	private class AsynchTwitter extends AsyncTask<Void, Void, List<twitter4j.Status>>
 	{
-		// If there is new tweets to add
-		if (result != null && result.size() > 0)
+		private static final String LOG_TAG_ASYNCH_TWITTER = "AsynchTwitter";
+
+		/**
+		 * Get the Twitter timelines using the twitter4j library.
+		 * Add the newest Tweets to the front of the list.
+		 * Get each tweet's user profile image and store it. This reduces the network traffic.
+		 * Use a least recently used strategy to remove unused profile images if the list gets to
+		 * big.
+		 * 
+		 * @param params
+		 *            We don't have any input values, thus Void
+		 * @return the list of tweets received.
+		 */
+		@Override
+		protected List<twitter4j.Status> doInBackground(Void... params)
 		{
-			// Add to the top of the list, and scroll the list view to the top position
-			adapter.addTweetsToTop(result);
 
-			// Let the user know that the update is complete
-			Toast.makeText(fragmentContext, "Update Complete", Toast.LENGTH_LONG).show();
+			if (verifyCredentials())
+			{
+				// Get the user and mentions timeline, then merge them into one sorted timeline
+				List<twitter4j.Status> tweets = getTimelines();
+
+				doProfileImages(tweets);
+
+				cleanup(tweets);
+
+				return tweets;
+			}
+			else
+			{
+				Log.d(LOG_TAG_ASYNCH_TWITTER, "Invalid Twitter Credentials!");
+				return null;
+			}
 		}
-		else
+
+		/**
+		 * Test whether the twitter keys, secrets and tokens are valid before getting timelines
+		 * 
+		 * @return True if twitter credentials are valid else false
+		 */
+		private boolean verifyCredentials()
 		{
-			Toast.makeText(fragmentContext, "No new Tweets", Toast.LENGTH_LONG).show();
+			try
+			{
+				twitter.verifyCredentials();
+				return true;
+			}
+			catch (TwitterException e)
+			{
+				return false;
+			}
 		}
 
-		Log.d(LOG_TAG_TWITTER_HANDLER, "Update Complete");
+		private void cleanup(List<twitter4j.Status> tweets)
+		{
+			// Remove the least recently used profile images if the List gets to big
+			while (userID.size() > maxImageCount)
+			{
+				int i = userID.size() - 1;
+				userID.remove(i);
+				drawableProfileImage.remove(i);
+				userProfileImageURL.remove(i);
+			}
 
-		fragmentContext.findViewById(R.id.twitter_refreshBar).setVisibility(ProgressBar.GONE);
+			// Only save the newest tweets (don't let the tweet list get to long)
+			while (tweets.size() > maxTweetCount)
+			{
+				int i = tweets.size() - 1;
+				tweets.remove(i);
+			}
+
+			// Save some space
+			drawableProfileImage.trimToSize();
+			userID.trimToSize();
+			userProfileImageURL.trimToSize();
+
+		}
+
+		/**
+		 * Store a list of already retrieved profile images to reduce the network cost
+		 * Add the images to the front of the list. So that the last image in the list is
+		 * one that isn't
+		 * used a lot.
+		 * If the list gets to big, delete the least frequently used images (at the back of
+		 * the list)
+		 * 
+		 * @param tweets
+		 */
+		private void doProfileImages(List<twitter4j.Status> tweets)
+		{
+
+			for (twitter4j.Status tweet : tweets)
+			{
+				String imageURL = tweet.getUser().getOriginalProfileImageURL();
+				Drawable drawable;
+
+				int indexOf = userID.indexOf(tweet.getUser().getId());
+				// not in list
+				if (indexOf == -1)
+				{
+					Log.d(LOG_TAG_ASYNCH_TWITTER, "NEW PROFILE IMAGES");
+
+					drawable = getUserProfileImage(imageURL);
+
+					// Add the image to the front of the list.
+					drawableProfileImage.add(0, drawable);
+					userProfileImageURL.add(0, imageURL);
+
+					// Add user to the front of the list
+					userID.add(0, tweet.getUser().getId());
+				}
+				else
+				{
+					// Check if there is a new profile image
+					if (!userProfileImageURL.contains(imageURL))
+					{
+						// Get new profile image
+						drawable = getUserProfileImage(imageURL);
+
+						Log.d(LOG_TAG_ASYNCH_TWITTER, "NEW cache PROFILE IMAGE");
+					}
+					else
+					{
+						// Get old image, so we can move it to the front of the list
+						drawable = drawableProfileImage.get(indexOf);
+						imageURL = userProfileImageURL.get(indexOf);
+
+						Log.d(LOG_TAG_ASYNCH_TWITTER, "move image to front");
+					}
+
+					// Move the image to the front of the list.
+					if (indexOf != 0)
+					{
+						drawableProfileImage.remove(indexOf);
+						drawableProfileImage.add(0, drawable);
+
+						userProfileImageURL.remove(indexOf);
+						userProfileImageURL.add(0, imageURL);
+
+						long tempID = userID.get(indexOf);
+						userID.remove(indexOf);
+						userID.add(0, tempID);
+					}
+				}
+
+			}
+
+		}
+
+		private Drawable getUserProfileImage(String imageURL)
+		{
+			URL url;
+			InputStream content;
+			Drawable result = null;
+			try
+			{
+				// Get the users profile image
+				url = new URL(imageURL);
+
+				content = (InputStream) url.openStream();
+				result = Drawable.createFromStream(content, "src");
+			}
+			catch (IOException e)
+			{
+				Log.e(LOG_TAG_ASYNCH_TWITTER, "Error retrieving profile image", e);
+			}
+
+			return result;
+		}
+
+		private List<twitter4j.Status> getTimelines()
+		{
+			List<twitter4j.Status> result = null;
+
+			List<twitter4j.Status> userTimeline = getUserTimeline();
+			List<twitter4j.Status> mentionsTimeline = getMentionsTimeline();
+
+			result = mergeAndSort(userTimeline, mentionsTimeline);
+
+			return result;
+		}
+
+		private List<twitter4j.Status> mergeAndSort(List<twitter4j.Status> userTimeline,
+							List<twitter4j.Status> mentionsTimeline)
+		{
+			// Add both timelines into one.
+			// Sort from most recent to least recently posted tweet.
+			List<twitter4j.Status> result = new ArrayList<twitter4j.Status>(userTimeline);
+
+			for (twitter4j.Status tweet : mentionsTimeline)
+			{
+				Log.d(LOG_TAG_ASYNCH_TWITTER, "comparing: " + tweet.getId());
+
+				for (int i = 0; i <= result.size(); i++)
+				{
+					if (i == result.size())
+					{
+						// List is empty or we reached the end of the list, so add normally
+						Log.d(LOG_TAG_ASYNCH_TWITTER, "Added: " + tweet.getId());
+						result.add(tweet);
+						break;
+					}
+					else if (result.get(i).getCreatedAt().compareTo(tweet.getCreatedAt()) < 0)
+					{
+						// Add the tweet in its correct position (sort by date)
+						Log.d(LOG_TAG_ASYNCH_TWITTER, "Added: " + tweet.getId() + " to: " + i);
+						result.add(i, tweet);
+						break;
+					}
+				}
+			}
+			return result;
+		}
+
+		/**
+		 * 
+		 * @return
+		 */
+		private List<twitter4j.Status> getUserTimeline()
+		{
+			List<twitter4j.Status> result = null;
+
+			try
+			{
+				// Get default amount of tweets for beginning
+				if (sinceUserTimelineID == -1)
+				{
+					result = twitter.getUserTimeline();
+					Log.d(LOG_TAG_ASYNCH_TWITTER, "new user timeline");
+				}
+				else
+				// Only get newest tweets
+				{
+					Paging paging = new Paging(1, timelineSize).sinceId(sinceUserTimelineID);
+					result = twitter.getUserTimeline(paging);
+					Log.d(LOG_TAG_ASYNCH_TWITTER, "caching user timeline");
+				}
+
+				// Set the sinceID to the first(newest) Tweet ID, so that in the future we only get
+				// newer tweets
+				if (result.size() > 0)
+					sinceUserTimelineID = result.get(0).getId();
+			}
+			catch (TwitterException e)
+			{
+				result = null;
+			}
+
+			return result;
+		}
+
+		/**
+		 * 
+		 * @return
+		 */
+		private List<twitter4j.Status> getMentionsTimeline()
+		{
+			List<twitter4j.Status> result = null;
+
+			try
+			{
+				if (sinceMentionsTimelineID == -1)
+				{
+					result = twitter.getMentionsTimeline();
+					Log.d(LOG_TAG_ASYNCH_TWITTER, "new mentions");
+				}
+				else
+				{
+					Paging paging = new Paging(1, 20).sinceId(sinceMentionsTimelineID);
+					result = twitter.getMentionsTimeline(paging);
+					Log.d(LOG_TAG_ASYNCH_TWITTER, "caching mentions");
+				}
+
+				// Set the sinceID to the first(newest) Tweet ID, so that in the future we only get
+				// newer tweets
+				if (result.size() > 0)
+					sinceMentionsTimelineID = result.get(0).getId();
+
+			}
+			catch (TwitterException e)
+			{
+				result = null;
+			}
+
+			return result;
+		}
+
+		/**
+		 * Add the new tweets to the top of the list and re-enable the refresh button.
+		 * 
+		 * @param result
+		 *            The list of tweets received from the doInBackground() function
+		 */
+		@Override
+		protected void onPostExecute(List<twitter4j.Status> result)
+		{
+			// If there is new tweets to add
+			if (result != null && result.size() > 0)
+			{
+				// Add to the top of the list, and scroll the list view to the top position
+				adapter.addTweetsToTop(result);
+				
+				ListView list = (ListView) fragmentContext.findViewById(android.R.id.list);
+				list.smoothScrollToPosition(0);
+				
+				// Let the user know that the update is complete
+				Toast.makeText(fragmentContext, "Update Complete", Toast.LENGTH_LONG).show();
+			}
+			else
+			{
+				Toast.makeText(fragmentContext, "No new Tweets", Toast.LENGTH_LONG).show();
+			}
+
+			Log.d(LOG_TAG_TWITTER_HANDLER, "Update Complete");
+
+			fragmentContext.findViewById(R.id.twitter_refreshBar).setVisibility(ProgressBar.GONE);
+		}
 	}
-
 }
