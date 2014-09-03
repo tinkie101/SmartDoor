f6373641155b5b8ed0ad7872909cbb7998f3ed07
eduanb <bekker.eduan@gmail.com>
2014-09-03 14:53:25 +0200

    Moved Camera Fragment to Facial Detection Package

--- Source/src/za/co/zebrav/facerecognition/FaceRecognizeCameraFragment.java
+++ Source/src/za/co/zebrav/facerecognition/FaceRecognizeCameraFragment.java
@@ -1,4 +1,4 @@
-package za.co.zebrav.smartdoor;
+package za.co.zebrav.facerecognition;
 
 import static org.bytedeco.javacpp.helper.opencv_objdetect.cvHaarDetectObjects;
 import static org.bytedeco.javacpp.opencv_core.IPL_DEPTH_8U;
