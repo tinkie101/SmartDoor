409fde02b0aa2e869a43e091860fd17f290ee96c
Zuhnja <u12040593@tuks.co.za>
2014-09-17 15:02:09 +0200

    Test if I can finally be able to commit and pull again. (Added comments to LastPK)

--- Source/src/za/co/zebrav/smartdoor/database/LastPK.java
+++ Source/src/za/co/zebrav/smartdoor/database/LastPK.java
@@ -4,11 +4,19 @@ public class LastPK
 {
 	private int pk = 0;
 	
+	/**
+	 * This is the constructor, set PK
+	 * @param pk
+	 */
 	public LastPK(int pk)
 	{
 		this.pk = pk;
 	}
 	
+	/**
+	 * Getter function
+	 * @return PK
+	 */
 	public int getPK()
 	{
 		return pk;
