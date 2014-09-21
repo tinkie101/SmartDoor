617378a7ff3d036c2529585b6284ce7f2041b326
eduanb <bekker.eduan@gmail.com>
2014-09-21 18:19:47 +0200

    Fixed add face not saving

--- Source/src/za/co/zebrav/facerecognition/AddFaceView.java
+++ Source/src/za/co/zebrav/facerecognition/AddFaceView.java
@@ -12,40 +12,38 @@ class AddFaceView extends FaceView
 	private static final String TAG = "AddFaceView";
 	private int count = 0;
 	private static final int classifierCount = 3;
-	private static final String[] classifierFiles = { "haarcascade_frontalface_alt.xml", "haarcascade_eye.xml",	"haarcascade_nose.xml" };
+	private static final String[] classifierFiles = { "haarcascade_frontalface_alt.xml", "haarcascade_eye.xml",
+						"haarcascade_nose.xml" };
 	private static final int colors[] = { Color.RED, Color.GREEN, Color.BLUE };
+
 	public AddFaceView(Activity activity) throws IOException
 	{
 		super(activity);
 		initialiseClassifiers(classifierCount, colors, classifierFiles);
 	}
 
-	
 	@Override
 	protected void handleDetected(byte[] data, int width, int height)
 	{
-		if (runnables[0].getObjects().total() == 1 && personRecognizer.canPredict())
+		if (runnables[0].getObjects().total() == 1 && runnables[1].getObjects().total() == 2
+							&& runnables[2].getObjects().total() == 1)
 		{
-			if (runnables[0].getObjects().total() == 1 && runnables[1].getObjects().total() == 2
-								&& runnables[2].getObjects().total() == 1)
+			ImageTools.saveImageAsPNG(ImageTools.getGreyImage(data, width, height, 1), uID + "-" + count, activity);
+			Log.d(TAG, "Saved ID:" + uID + " Number: " + count + " .");
+			count++;
+			if (count == 5)
 			{
-				ImageTools.saveImageAsPNG(ImageTools.getGreyImage(data, width, height, 1), uID + "-" + count, activity);
-				Log.d(TAG, "Saved ID:"+ uID +" Number: " + count + " .");
-				count++;
-				if (count == 5)
-				{
-					((AddUserActivity)activity).switchFragToStep3();
-				}
+				((AddUserActivity) activity).switchFragToStep3();
 			}
 		}
 	}
-	
+
 	@Override
 	protected int getColor(int id)
 	{
 		return colors[id];
 	}
-	
+
 	@Override
 	protected int getClassifierCount()
 	{
