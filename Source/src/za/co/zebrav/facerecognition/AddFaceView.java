a5daf7622ebb812303feebc52425a636c15a234f
eduanb <bekker.eduan@gmail.com>
2014-10-01 18:11:14 +0200

    Eyes and nose detection only on face and not on full image.

--- Source/src/za/co/zebrav/facerecognition/AddFaceView.java
+++ Source/src/za/co/zebrav/facerecognition/AddFaceView.java
@@ -6,6 +6,8 @@ import org.bytedeco.javacpp.Loader;
 import org.bytedeco.javacpp.opencv_objdetect;
 import org.bytedeco.javacpp.opencv_core.CvMemStorage;
 
+import org.bytedeco.javacpp.opencv_core.Mat;
+
 import za.co.zebrav.smartdoor.database.AddUserActivity;
 import android.app.Activity;
 import android.app.Fragment;
@@ -24,7 +26,7 @@ class AddFaceView extends FaceView
 	 * List is kept to have access to their local variables.
 	 */
 	private ClassifierRunnable[] runnables;
-	
+
 	public AddFaceView(Activity activity, Fragment fragment) throws IOException
 	{
 		super(activity, fragment);
@@ -42,10 +44,10 @@ class AddFaceView extends FaceView
 
 		runnables[0] = new FaceClassifierRunnable(storage, activity.getCacheDir());
 		threads[0] = new Thread(runnables[0], "" + 0);
-		
+
 		runnables[1] = new EyesClassifierRunnable(storage, activity.getCacheDir());
 		threads[1] = new Thread(runnables[1], "" + 1);
-		
+
 		runnables[2] = new NoseClassifierRunnable(storage, activity.getCacheDir());
 		threads[2] = new Thread(runnables[2], "" + 2);
 	}
@@ -60,7 +62,7 @@ class AddFaceView extends FaceView
 			ImageTools.saveImageAsPNG(ImageTools.getGreyMatImage(data, width, height, 1), uID + "-" + count, activity);
 			Log.d(TAG, "Saved ID:" + uID + " Number: " + count + " .");
 			count++;
-			((AddCameraFragment)fragment).setProgress(25*count);
+			((AddCameraFragment) fragment).setProgress(25 * count);
 			if (count == 5)
 			{
 				((AddUserActivity) activity).switchFragToStep3();
@@ -86,19 +88,57 @@ class AddFaceView extends FaceView
 		return runnables;
 	}
 
+	boolean savedImage = false;
+
 	@Override
 	protected void runClassifiers()
 	{
-		for (int i = 0; i < threads.length; i++)
+		getRunnables()[0].setGrayImage(grayImage);
+		threads[0].run();
+		try
 		{
-				getRunnables()[i].setGrayImage(grayImage);
-				threads[i].run();
+			threads[0].join();
 		}
-		for (int i = 0; i < threads.length; i++)
+		catch (InterruptedException e1)
 		{
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		if (getRunnables()[0].getTotalDetected() > 0)
+		{
+			Log.d(TAG, "begin creating result");
+			int beginx = (int) (getRunnables()[0].getObjects().x() - ((getRunnables()[0].getObjects().width()) * 0.1));
+			int beginy = (int) (getRunnables()[0].getObjects().y() - ((getRunnables()[0].getObjects().height()) * 0.1));
+			
+			
+			int endx = (int) (getRunnables()[0].getObjects().width() + ((getRunnables()[0].getObjects().width()) * 0.1));
+			int endy = (int) (getRunnables()[0].getObjects().height() + ((getRunnables()[0].getObjects().height()) * 0.1));
+			
+			
+			Mat result = grayImage.rowRange(beginy, endx);
+			result = result.colRange(beginx, endy);
+			
+			
+			 //Mat result = new Mat(getRunnables()[0].getObjects());
+			 Log.d(TAG, "done creating result");
+//			if (!savedImage)
+//			{
+//				ImageTools.saveImageAsPNG(result, "test", activity);
+//				Log.d(TAG, "done writing image result");
+//				savedImage = true;
+//			}
+
+			getRunnables()[1].setGrayImage(result);
+			threads[1].run();
+
+			getRunnables()[2].setGrayImage(result);
+			threads[2].run();
+
 			try
 			{
-				threads[i].join();
+				threads[1].join();
+				threads[2].join();
 			}
 			catch (InterruptedException e)
 			{
