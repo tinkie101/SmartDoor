76ed1414e2bfa59cbf41cffabaae4dc081655a3d
eduanb <bekker.eduan@gmail.com>
2014-10-07 19:29:12 +0200

    Added auto logout.

--- Source/src/za/co/zebrav/smartdoor/MainActivity.java
+++ Source/src/za/co/zebrav/smartdoor/MainActivity.java
@@ -6,7 +6,6 @@
 //This is the main activity for the Smart Door Application.
 package za.co.zebrav.smartdoor;
 
-
 import java.util.List;
 
 import za.co.zebrav.smartdoor.R.id;
@@ -21,15 +20,17 @@ import android.app.FragmentTransaction;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
-public class MainActivity extends AbstractActivity 
+public class MainActivity extends AbstractActivity
 {
 	private static final String TAG = "MainActivity";
 	private long back_pressed;
@@ -41,6 +42,8 @@ public class MainActivity extends AbstractActivity
 	private boolean loggedIn = false;
 	private Fragment twitterFragment;
 	private PersonRecognizer personRecognizer;
+	private CountDownTimer logoutTimer;
+
 	public PersonRecognizer getPersonRecognizer()
 	{
 		return personRecognizer;
@@ -49,29 +52,55 @@ public class MainActivity extends AbstractActivity
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
-		super.onCreate(savedInstanceState);	
+		super.onCreate(savedInstanceState);
 		Log.d(TAG, "onCreate");
 		setContentView(R.layout.activity_main);
 
 		loadDefaultSettings();
-		
+
 		switchToTwitterFragment();
 
 		userCommands = new UserCommands(this);
-		
+
 		// add slider menu
 		sliderMenu = new CustomMenu(this, (ListView) findViewById(R.id.drawer_list),
 							(DrawerLayout) findViewById(R.id.drawer_layout), getResources().getStringArray(
 												R.array.mainMenuOptions));
-		
-		//create alert
+
+		// create alert
 		alert = new AlertDialog.Builder(this);
-		
+
 		identifyVoiceFragment = new IdentifyVoiceFragment();
 		searchCameraFragment = new SearchCameraFragment();
+		logoutTimer = new CountDownTimer(30000, 10000)
+		{
+			public void onFinish()
+			{
+				speakOut("Logging out after due to inactivity.");
+				logout();
+			}
+
+			@Override
+			public void onTick(long millisUntilFinished)
+			{
+				Log.d(TAG, "Time to logout:" + millisUntilFinished);
+				
+			}
+		};
 		switchToCamera();
 	}
-	
+
+	@Override
+	public void onUserInteraction()
+	{
+		super.onUserInteraction();
+		if (loggedIn)
+		{
+			logoutTimer.cancel();
+			logoutTimer.start();
+		}
+	}
+
 	@Override
 	public void onBackPressed()
 	{
@@ -81,137 +110,145 @@ public class MainActivity extends AbstractActivity
 			Toast.makeText(getBaseContext(), "Press again to exit", Toast.LENGTH_SHORT).show();
 		back_pressed = System.currentTimeMillis();
 	}
-	
+
 	private void loadDefaultSettings()
 	{
 		String PREFS_NAME = getResources().getString((R.string.settingsFileName));
 		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-		
-		//Face settings
+
+		// Face settings
 		String face_TrainPhotoNum = settings.getString("face_TrainPhotoNum", "NOT");
-		if(face_TrainPhotoNum.equals("NOT"))//not set, set
+		if (face_TrainPhotoNum.equals("NOT"))// not set, set
 		{
 			SharedPreferences.Editor editor = settings.edit();
-			
-		    editor.putString("face_TrainPhotoNum", getResources().getString((R.string.face_TrainPhotoNum)));
-		    editor.commit();
-		    
-		    editor.putString("face_RecogPhotoNum", getResources().getString((R.string.face_RecogPhotoNum)));
-		    editor.commit();
-		    
-		    editor.putString("face_recognizerThreshold", getResources().getString((R.string.face_recognizerThreshold)));
-		    editor.commit();
-		    
-		    editor.putString("face_ImageScale", getResources().getString((R.string.face_ImageScale)));
-		    editor.commit();
-		    
-		    editor.putString("face_faceRecognizerAlgorithm", getResources().getString((R.string.face_faceRecognizerAlgorithm)));
-		    editor.commit();
-		    
-		    editor.putString("face_detectEyes", getResources().getString((R.string.face_detectEyes)));
-		    editor.commit();
-		    
-		    editor.putString("face_detectNose", getResources().getString((R.string.face_detectNose)));
-		    editor.commit();
-		    
-		    editor.putString("face_resolution", getResources().getString((R.string.face_resolution)));
-		    editor.commit();
-		    
-		    Log.d("missing", "main: "+ getResources().getString((R.string.face_GroupRectangleThreshold)));
-		    editor.putString("face_GroupRectangleThreshold", getResources().getString((R.string.face_GroupRectangleThreshold)));
-		    editor.commit();
+
+			editor.putString("face_TrainPhotoNum", getResources().getString((R.string.face_TrainPhotoNum)));
+			editor.commit();
+
+			editor.putString("face_RecogPhotoNum", getResources().getString((R.string.face_RecogPhotoNum)));
+			editor.commit();
+
+			editor.putString("face_recognizerThreshold", getResources().getString((R.string.face_recognizerThreshold)));
+			editor.commit();
+
+			editor.putString("face_ImageScale", getResources().getString((R.string.face_ImageScale)));
+			editor.commit();
+
+			editor.putString("face_faceRecognizerAlgorithm",
+								getResources().getString((R.string.face_faceRecognizerAlgorithm)));
+			editor.commit();
+
+			editor.putString("face_detectEyes", getResources().getString((R.string.face_detectEyes)));
+			editor.commit();
+
+			editor.putString("face_detectNose", getResources().getString((R.string.face_detectNose)));
+			editor.commit();
+
+			editor.putString("face_resolution", getResources().getString((R.string.face_resolution)));
+			editor.commit();
+
+			Log.d("missing", "main: " + getResources().getString((R.string.face_GroupRectangleThreshold)));
+			editor.putString("face_GroupRectangleThreshold",
+								getResources().getString((R.string.face_GroupRectangleThreshold)));
+			editor.commit();
 		}
-		
-		//Twitter settings
+
+		// Twitter settings
 		String twitter_Key = settings.getString("twitter_Key", "NOT");
-		if(twitter_Key.equals("NOT"))//not set, set
+		if (twitter_Key.equals("NOT"))// not set, set
 		{
 			SharedPreferences.Editor editor = settings.edit();
-			
-		    editor.putString("twitter_Key", getResources().getString((R.string.twitter_Key)));
-		    editor.commit();
-		    
-		    editor.putString("twitter_Secret", getResources().getString((R.string.twitter_Secret)));
-		    editor.commit();
-		    
-		    editor.putString("twitter_TokenKey", getResources().getString((R.string.twitter_TokenKey)));
-		    editor.commit();
-		    
-		    editor.putString("twitter_TokenSecret", getResources().getString((R.string.twitter_TokenSecret)));
-		    editor.commit();
+
+			editor.putString("twitter_Key", getResources().getString((R.string.twitter_Key)));
+			editor.commit();
+
+			editor.putString("twitter_Secret", getResources().getString((R.string.twitter_Secret)));
+			editor.commit();
+
+			editor.putString("twitter_TokenKey", getResources().getString((R.string.twitter_TokenKey)));
+			editor.commit();
+
+			editor.putString("twitter_TokenSecret", getResources().getString((R.string.twitter_TokenSecret)));
+			editor.commit();
 		}
-		
-		//serverSettings
+
+		// serverSettings
 		String server_IP = settings.getString("server_IP", "NOT");
-		if(server_IP.equals("NOT"))//not set, set
+		if (server_IP.equals("NOT"))// not set, set
 		{
 			SharedPreferences.Editor editor = settings.edit();
-			
-		    editor.putString("server_IP", getResources().getString((R.string.server_IP)));
-		    editor.commit();
-		    
-		    editor.putString("server_Port", getResources().getString((R.string.server_Port)));
-		    editor.commit();
+
+			editor.putString("server_IP", getResources().getString((R.string.server_IP)));
+			editor.commit();
+
+			editor.putString("server_Port", getResources().getString((R.string.server_Port)));
+			editor.commit();
 		}
 	}
-	
+
 	protected void onResume()
 	{
 		super.onResume();
 		back_pressed = System.currentTimeMillis();
 		String settingsFile = getResources().getString(R.string.settingsFileName);
-		int photosPerPerson = Integer.parseInt(getSharedPreferences(settingsFile, 0).getString(
-							"face_TrainPhotoNum", "5"));
+		int photosPerPerson = Integer.parseInt(getSharedPreferences(settingsFile, 0).getString("face_TrainPhotoNum",
+							"5"));
 		int algorithm = Integer.parseInt(getSharedPreferences(settingsFile, 0).getString(
 							"face_faceRecognizerAlgorithm", "1"));
-		int threshold = Integer.parseInt(getSharedPreferences(settingsFile, 0).getString(
-							"face_recognizerThreshold", "0"));
+		int threshold = Integer.parseInt(getSharedPreferences(settingsFile, 0).getString("face_recognizerThreshold",
+							"0"));
 		personRecognizer = new PersonRecognizer(this, photosPerPerson, algorithm, threshold);
-		if(!loggedIn)
+		if (!loggedIn)
 		{
-			//user = null;
+			// user = null;
 			this.currentFragment = "advanced";
 			this.switchToCamera();
 		}
 	}
-	
+
 	/**
 	 * When the user presses the toggle login button, the fragment part underneath the button must change.
 	 * When the current fragment is the advanced part (camera or voice authentication), it switches to a manual login
 	 * and vice versa
+	 * 
 	 * @param v
 	 */
 	public void switchLogin(View v)
 	{
 		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-		
-		if(currentFragment.equals("manual"))
+
+		if (currentFragment.equals("manual"))
 		{
 			searchCameraFragment = new SearchCameraFragment();
-			fragmentTransaction.replace(R.id.layoutToReplaceFromMain , searchCameraFragment);
+			fragmentTransaction.replace(R.id.layoutToReplaceFromMain, searchCameraFragment);
 			currentFragment = "advanced";
 		}
-		else if(this.currentFragment.equals("advanced"))
+		else if (this.currentFragment.equals("advanced"))
 		{
 			manualFrag = new ManualLoginFragment();
-			fragmentTransaction.replace(R.id.layoutToReplaceFromMain , manualFrag);
+			fragmentTransaction.replace(R.id.layoutToReplaceFromMain, manualFrag);
 			currentFragment = "manual";
 		}
-		
+
 		fragmentTransaction.commit();
 	}
-	
+
 	public void logout()
 	{
+		Log.d(TAG, "Logout called.");
+		loggedIn = false;
+		logoutTimer.cancel();
 		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 		searchCameraFragment = new SearchCameraFragment();
-		fragmentTransaction.replace(R.id.layoutToReplaceFromMain , searchCameraFragment);
+		fragmentTransaction.replace(R.id.layoutToReplaceFromMain, searchCameraFragment);
 		currentFragment = "advanced";
 		fragmentTransaction.commit();
 		Button button = (Button) findViewById(id.switchLoginButton);
 		button.setVisibility(View.VISIBLE);
+		ProgressBar soundLevel = (ProgressBar) findViewById(R.id.progressBar1);
+		soundLevel.setProgress(0);
 	}
-	
+
 	public void switchToLoggedInFrag()
 	{
 		Button button = (Button) findViewById(R.id.switchLoginButton);
@@ -219,12 +256,12 @@ public class MainActivity extends AbstractActivity
 		loggedInFragment = new LoggedInFragment();
 
 		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-		fragmentTransaction.replace(R.id.layoutToReplaceFromMain , loggedInFragment);
+		fragmentTransaction.replace(R.id.layoutToReplaceFromMain, loggedInFragment);
 		fragmentTransaction.commit();
 		this.loggedIn = true;
+		logoutTimer.start();
 	}
-	
-	
+
 	/**
 	 * Alerts the specified message in dialogue box.
 	 */
@@ -232,63 +269,63 @@ public class MainActivity extends AbstractActivity
 	{
 		alert.setTitle("Alert").setMessage(message).setNeutralButton("OK", null).show();
 	}
-	
+
 	IdentifyVoiceFragment identifyVoiceFragment;
 	SearchCameraFragment searchCameraFragment;
-	
+
 	public void switchToCamera()
 	{
 		searchCameraFragment = new SearchCameraFragment();
 
 		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-		fragmentTransaction.replace(R.id.layoutToReplaceFromMain , searchCameraFragment);
+		fragmentTransaction.replace(R.id.layoutToReplaceFromMain, searchCameraFragment);
 		fragmentTransaction.commit();
 		this.loggedIn = false;
 	}
-	
+
 	public void switchToVoice(int id)
 	{
 		List<Object> users = getDatabase().load(new User(null, null, null, null, null, id, null));
 		activityUser = (User) users.get(0);
-		
+
 		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-		fragmentTransaction.replace(R.id.layoutToReplaceFromMain , identifyVoiceFragment);
+		fragmentTransaction.replace(R.id.layoutToReplaceFromMain, identifyVoiceFragment);
 		fragmentTransaction.commit();
 	}
-	
+
 	public void switchToTwitterSetup()
 	{
 		TwitterSetupFragment t = new TwitterSetupFragment();
 
 		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-		fragmentTransaction.replace(R.id.layoutToReplaceFromMain , t);
+		fragmentTransaction.replace(R.id.layoutToReplaceFromMain, t);
 		fragmentTransaction.commit();
 	}
-	
+
 	public void switchToSettingsFragment()
 	{
 		SettingsFragment t = new SettingsFragment();
 
 		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-		fragmentTransaction.replace(R.id.layoutToReplaceFromMain , t);
+		fragmentTransaction.replace(R.id.layoutToReplaceFromMain, t);
 		fragmentTransaction.commit();
 	}
-	
+
 	public void switchToTwitterFragment()
 	{
 		FragmentManager fragmentManager = getFragmentManager();
-	    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-	    twitterFragment = new TwitterFragment();
-	    fragmentTransaction.replace(R.id.twitterFragment, twitterFragment);
-	    fragmentTransaction.commit();
+		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+		twitterFragment = new TwitterFragment();
+		fragmentTransaction.replace(R.id.twitterFragment, twitterFragment);
+		fragmentTransaction.commit();
 	}
-	
+
 	public void tryTwitter()
 	{
 		((TwitterFragment) twitterFragment).tryTwitter();
 	}
 
-	//-------------------------------------------------------------------------------------Menu
+	// -------------------------------------------------------------------------------------Menu
 	@Override
 	protected void onPostResume()
 	{
