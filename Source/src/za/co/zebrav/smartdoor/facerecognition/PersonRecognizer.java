395b755af8ac050749e320df04097b5579f225de
eduanb <bekker.eduan@gmail.com>
2014-10-16 23:44:24 +0200

    Updated syntax

--- Source/src/za/co/zebrav/smartdoor/facerecognition/PersonRecognizer.java
+++ Source/src/za/co/zebrav/smartdoor/facerecognition/PersonRecognizer.java
@@ -17,7 +17,8 @@ import org.bytedeco.javacpp.opencv_core.MatVector;
 
 import android.util.Log;
 
-public class PersonRecognizer {
+public class PersonRecognizer
+{
 	private static final String LOG_TAG = "PersonRecognizer";
 	public static final int LBPHFaceRecognizer = 1;
 	public static final int FisherFaceRecognizer = 2;
@@ -55,26 +56,27 @@ public class PersonRecognizer {
 	 * @param dataDir
 	 *            Location where these photos are stored
 	 */
-	public PersonRecognizer(int photosPerPerson, int algorithm, int threshold,
-			LinkedList<Integer> IDList, File dataDir) {
+	public PersonRecognizer(int photosPerPerson, int algorithm, int threshold, LinkedList<Integer> IDList, File dataDir)
+	{
 		this.photosPerPerson = photosPerPerson;
-		switch (algorithm) {
-		case 1:
-			faceRecognizer = createLBPHFaceRecognizer();
-			Log.d(LOG_TAG, "Created LBPHFaceRecognizer");
-			break;
-		case 2:
-			faceRecognizer = createFisherFaceRecognizer();
-			Log.d(LOG_TAG, "Created FisherFaceRecognizer");
-			break;
-		case 3:
-			faceRecognizer = createEigenFaceRecognizer();
-			Log.d(LOG_TAG, "Created EigenFaceRecognizer");
-			break;
-		default:
-			faceRecognizer = createLBPHFaceRecognizer();
-			Log.d(LOG_TAG, "Created LBPHFaceRecognizer");
-			break;
+		switch (algorithm)
+		{
+			case 1:
+				faceRecognizer = createLBPHFaceRecognizer();
+				Log.d(LOG_TAG, "Created LBPHFaceRecognizer");
+				break;
+			case 2:
+				faceRecognizer = createFisherFaceRecognizer();
+				Log.d(LOG_TAG, "Created FisherFaceRecognizer");
+				break;
+			case 3:
+				faceRecognizer = createEigenFaceRecognizer();
+				Log.d(LOG_TAG, "Created EigenFaceRecognizer");
+				break;
+			default:
+				faceRecognizer = createLBPHFaceRecognizer();
+				Log.d(LOG_TAG, "Created LBPHFaceRecognizer");
+				break;
 		}
 		faceRecognizer.set("threshold", threshold);
 		isTrained = initialiseRecogniser(IDList, dataDir);
@@ -91,9 +93,11 @@ public class PersonRecognizer {
 	 *            The context needed for the database.
 	 * @return True when preconditions were met, False otherwise.
 	 */
-	private boolean initialiseRecogniser(LinkedList<Integer> list, File dataDir) {
+	private boolean initialiseRecogniser(LinkedList<Integer> list, File dataDir)
+	{
 		// Check precondition 1
-		if (list == null || list.size() < 2) {
+		if (list == null || list.size() < 2)
+		{
 			Log.e(LOG_TAG, "List less than 2.");
 			return false;
 		}
@@ -103,17 +107,19 @@ public class PersonRecognizer {
 		MatVector images = new MatVector(list.size() * photosPerPerson);
 		// loop through each user in database
 		int i = 0; // used to keep count of amount of images and labels added
-		for (Integer id : list) {
+		for (Integer id : list)
+		{
 			// loop through each photo of the specific user
-			for (int j = 0; j < photosPerPerson; j++) {
+			for (int j = 0; j < photosPerPerson; j++)
+			{
 				// add the user's id to the back
 				labelsBuf.put(i, id);
 
 				// Get file
-				File file = new File(dataDir + "/photos/", id + "-" + j
-						+ ".png");
+				File file = new File(dataDir + "/photos/", id + "-" + j + ".png");
 				// Check precondition 2
-				if (!file.exists()) {
+				if (!file.exists())
+				{
 					Log.e(LOG_TAG, file.toString() + " does not exist!");
 					return false;
 				}
@@ -144,24 +150,29 @@ public class PersonRecognizer {
 	 *            List of labels to train on.
 	 * @return True when preconditions were met, False otherwise.
 	 */
-	private boolean train(MatVector images, Mat labels) {
+	private boolean train(MatVector images, Mat labels)
+	{
 		// Check precondition 1
-		if (images == null || images.capacity() == 0) {
+		if (images == null || images.capacity() == 0)
+		{
 			Log.e(LOG_TAG, "Images must be a valid list.");
 			return false;
 		}
 		// Check precondition 2
-		if (labels == null || labels.capacity() == 0) {
+		if (labels == null || labels.capacity() == 0)
+		{
 			Log.e(LOG_TAG, "Labels must be a valid list.");
 			return false;
 		}
 		// Check precondition 3
-		if (labels.rows() != images.size()) {
+		if (labels.rows() != images.size())
+		{
 			Log.e(LOG_TAG, "Labels must be the same length as images.");
 			return false;
 		}
 		// Check precondition 4
-		if (images.size() < 2) {
+		if (images.size() < 2)
+		{
 			Log.e(LOG_TAG, "Requires a minimum of 2 images to train on.");
 			return false;
 		}
@@ -173,8 +184,7 @@ public class PersonRecognizer {
 		// calculate and log training time
 		long temp = endTime - startTime;
 		double timeInSeconds = temp / (double) 1000;
-		Log.d(LOG_TAG, "Training on " + images.size() + " files took "
-				+ timeInSeconds + " seconds.");
+		Log.d(LOG_TAG, "Training on " + images.size() + " files took " + timeInSeconds + " seconds.");
 		// Successfully trained
 		return true;
 	}
@@ -185,7 +195,8 @@ public class PersonRecognizer {
 	 * @return True if it has been trained and it is able to predict, false
 	 *         otherwise.
 	 */
-	public boolean canPredict() {
+	public boolean canPredict()
+	{
 		return isTrained;
 	}
 
@@ -201,14 +212,17 @@ public class PersonRecognizer {
 	 * @return ID of the person that was recognized. If none was recognized -1
 	 *         will be returned.
 	 */
-	public int predict(Mat testImage) {
+	public int predict(Mat testImage)
+	{
 		// Test precondition 1
-		if (testImage == null || testImage.cols() == 0) {
+		if (testImage == null || testImage.cols() == 0)
+		{
 			certainty = -1;
 			return -1;
 		}
 		// Test precondition 2
-		if (!canPredict()) {
+		if (!canPredict())
+		{
 			certainty = -1;
 			return -1;
 		}
@@ -232,7 +246,8 @@ public class PersonRecognizer {
 	 * @return Certainty of last prediction. If none was predicted, or
 	 *         prediction is not available -1 will be returned.
 	 */
-	public double getCertainty() {
+	public double getCertainty()
+	{
 		return certainty;
 	}
 }
