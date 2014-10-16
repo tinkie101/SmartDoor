b90fee233fb5073211b56a46eeef936870541445
eduanb <bekker.eduan@gmail.com>
2014-10-16 16:07:34 +0200

    Finished Group Rectangle Unit test

--- SmartDoorTest/src/za/co/zebrav/smartdoor/test/ClassifierRunnableTest.java
+++ SmartDoorTest/src/za/co/zebrav/smartdoor/test/ClassifierRunnableTest.java
@@ -80,6 +80,62 @@ public class ClassifierRunnableTest extends TestCase
 		face.setObjects(test);
 		assertSame("Should be same as inithilised value",1, face.getTotalDetected());
 		face.rectangleGroup();
-		assertSame("After run should have same amount of rectangles",1, face.getTotalDetected());
+		assertSame("Only one rectangle so one should return",1, face.getTotalDetected());
+		
+		test = new Rect(2);
+		test.position(0).x(0).y(0).width(100).height(100);
+		test.position(1).x(0).y(0).width(100).height(100); 
+		face.setObjects(test);
+		assertSame("Should be same as inithilised value",2, face.getTotalDetected());
+		face.rectangleGroup();
+		assertSame("2 identical rectangles should be grouped to 1",1, face.getTotalDetected());
+		
+		test = new Rect(10);
+		test.position(0).x(0).y(0).width(100).height(100);
+		test.position(2).x(0).y(0).width(100).height(100);
+		test.position(3).x(0).y(0).width(100).height(100); 
+		test.position(4).x(0).y(0).width(100).height(100); 
+		test.position(5).x(0).y(0).width(100).height(100); 
+		test.position(6).x(0).y(0).width(100).height(100); 
+		test.position(7).x(0).y(0).width(100).height(100); 
+		test.position(8).x(0).y(0).width(100).height(100); 
+		test.position(9).x(0).y(0).width(100).height(100); 
+		face.setObjects(test);
+		assertSame("Should be same as inithilised value",10, face.getTotalDetected());
+		face.rectangleGroup();
+		assertSame("10 identical rectangles should be grouped to 1",1, face.getTotalDetected());
+		
+		test = new Rect(2);
+		test.position(0).x(0).y(0).width(100).height(100);
+		test.position(1).x(0).y(0).width(101).height(100); 
+		face.setObjects(test);
+		assertSame("Should be same as inithilised value",2, face.getTotalDetected());
+		face.rectangleGroup();
+		assertSame("2 very simular rectangles should group to 1",1, face.getTotalDetected());
+		
+		test = new Rect(2);
+		test.position(0).x(0).y(0).width(100).height(100);
+		test.position(1).x(10).y(10).width(110).height(110); 
+		face.setObjects(test);
+		assertSame("Should be same as inithilised value",2, face.getTotalDetected());
+		face.rectangleGroup();
+		assertSame("2 fairly simular rectangles should group to 1",1, face.getTotalDetected());
+		
+		test = new Rect(2);
+		test.position(0).x(0).y(0).width(100).height(100);
+		test.position(1).x(100).y(100).width(200).height(200); 
+		face.setObjects(test);
+		assertSame("Should be same as inithilised value",2, face.getTotalDetected());
+		face.rectangleGroup();
+		assertSame("2 completely different rectangles should not be grouped",2, face.getTotalDetected());
+		
+		face = new FaceClassifierRunnable(storage,cacheDir,Double.MAX_VALUE);
+		test = new Rect(2);
+		test.position(0).x(0).y(0).width(100).height(100);
+		test.position(1).x(100).y(100).width(200).height(200); 
+		face.setObjects(test);
+		assertSame("Should be same as inithilised value",2, face.getTotalDetected());
+		face.rectangleGroup();
+		assertSame("When threshold tends to inf it should group all rectangles",1, face.getTotalDetected());
 	}
 }
